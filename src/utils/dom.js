6a23ffab3a507bf6e587a667e34bea2fd979d3b2
savoygu <savoygu@gmail.com>
2017-09-14 13:31:54 +0800

    Add dom helper and remove travis cache config

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,36 +1,190 @@
 /**
+ * https://github.com/ElemeFE/element/blob/dev/src/utils/dom.js
  * DOM 操作
  */
-// 添加事件监听
-export const on = function (element, event, handler) {
-  document.addEventListener
-    ? element.addEventListener(event, handler, false)
-    : element.attachEvent('on' + event, handler)
+const SPECIAL_CHARS_REGEXP = /([:\-_]+(.))/g
+const MOZ_HACK_REGEXP = /^moz([A-Z])/
+const ieVersion = Number(document.documentMode)
+
+// 去除空格
+export const trim = function (string) {
+  return (string || '').replace(/^[\s\uFEFF]+|[\s\uFEFF]+$/g, '')
 }
+
+// 驼峰
+export const camelCase = function (name) {
+  return name.replace(SPECIAL_CHARS_REGEXP, function (_, separator, letter, offset) {
+    return offset ? letter.toUpperCase() : letter
+  }).replace(MOZ_HACK_REGEXP, 'Moz$1')
+}
+
+// 添加事件监听
+export const on = (function () {
+  if (document.addEventListener) {
+    return function (element, event, handler) {
+      if (element && event && handler) {
+        element.addEventListener(event, handler, false)
+      }
+    }
+  } else {
+    return function (element, event, handler) {
+      if (element && event && handler) {
+        element.attachEvent('on' + event, handler)
+      }
+    }
+  }
+})()
 
 // 移除事件监听
-export const off = function (element, event, handler) {
-  document.removeEventListener
-    ? element.removeEventListener(event, handler, false)
-    : element.detachEvent('on' + event, handler)
-}
+export const off = (function () {
+  if (document.removeEventListener) {
+    return function (element, event, handler) {
+      if (element && event) {
+        element.removeEventListener(event, handler, false)
+      }
+    }
+  } else {
+    return function (element, event, handler) {
+      if (element && event) {
+        element.detachEvent('on' + event, handler)
+      }
+    }
+  }
+})()
 
-// 添加 class
-export const addClass = function (elm, clz) {
-
-}
-
-// 移除 class
-export const removeClass = function (elm, clz) {
-
+export const once = function (el, event, fn) {
+  var listener = function () {
+    if (fn) {
+      fn.apply(this, arguments)
+    }
+    off(el, event, listener)
+  }
+  on(el, event, listener)
 }
 
 // 是否包含 class
-export const hasClass = function (elm, clz) {
-
+export const hasClass = function (el, cls) {
+  if (!el || !cls) return false
+  if (cls.indexOf(' ') !== -1) throw new Error('className should not contain space.')
+  if (el.classList) {
+    return el.classList.contains(cls)
+  } else {
+    return (' ' + el.className + ' ').indexOf(' ' + cls + ' ') > -1
+  }
 }
 
-// 设置 css
-export const css = function () {
+// 添加 class
+export const addClass = function (el, cls) {
+  if (!el) return
+  var curClass = el.className
+  var classes = (cls || '').split(' ')
 
+  for (var i = 0, len = classes.length; i < len; i++) {
+    var clsName = classes[i]
+    if (!clsName) continue
+
+    if (el.classList) { // IE9+
+      el.classList.add(clsName)
+    } else {
+      if (!hasClass(el, clsName)) {
+        curClass += ' ' + clsName
+      }
+    }
+  }
+  if (!el.classList) {
+    el.className = curClass
+  }
+}
+
+// 移除 class
+export const removeClass = function (el, cls) {
+  if (!el || !cls) return
+  var classes = cls.split(' ')
+  var curClass = ' ' + el.className + ' '
+
+  for (var i = 0, len = classes.length; i < len; i++) {
+    var clsName = classes[i]
+    if (!clsName) return
+
+    if (el.classList) {
+      el.classList.remove(clsName)
+    } else {
+      if (hasClass(el, clsName)) {
+        curClass = curClass.replace(' ' + clsName + ' ', ' ')
+      }
+    }
+  }
+  if (!el.classList) {
+    el.className = trim(curClass)
+  }
+}
+
+// 获取样式属性值
+export const getStyle = ieVersion < 9 ? function (element, styleName) {
+  if (!element || !styleName) return null
+  styleName = camelCase(styleName)
+  if (styleName === 'float') {
+    styleName = 'styleFloat'
+  }
+  try {
+    switch (styleName) {
+      case 'opacity':
+        try {
+          return element.filters.item('alpha').opacity / 100
+        } catch (e) {
+          return 1.0
+        }
+      default:
+        return (element.style[styleName] || element.currentStyle ? element.currentStyle[styleName] : null)
+    }
+  } catch (e) {
+    return element.style[styleName]
+  }
+} : function (element, styleName) {
+  if (!element || !styleName) return null
+  styleName = camelCase(styleName)
+  if (styleName === 'float') {
+    styleName = 'cssFloat'
+  }
+  try {
+    var computed = document.defaultView.getComputedStyle(element, '')
+    return element.style[styleName] || computed ? computed[styleName] : null
+  } catch (e) {
+    return element.style[styleName]
+  }
+}
+
+// 设置样式属性值
+export const setStyle = function (element, styleName, value) {
+  if (!element || !styleName) return
+
+  if (typeof styleName === 'object') {
+    for (var prop in styleName) {
+      if (styleName.hasOwnProperty(prop)) {
+        setStyle(element, prop, styleName[prop])
+      }
+    }
+  } else {
+    styleName = camelCase(styleName)
+    if (styleName === 'opacity' && ieVersion < 9) {
+      element.style.filter = isNaN(value) ? '' : 'alpha(opacity=' + value * 100 + ')'
+    } else {
+      element.style[styleName] = value
+    }
+  }
+}
+
+// http://harttle.com/2016/04/24/client-height-width.html
+// 获取窗口宽度 FireFox: document.body, Chrome/IE: document.documentElement
+export const getWinWidth = function () {
+  return window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth
+}
+
+// 获取窗口高度
+export const getWinHeight = function () {
+  return window.innerHeight ||
+    document.documentElement.clientHeight ||
+    document.body.clientHeight
 }
